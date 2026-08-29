f74dbe52dee09cc33ee0e95df56dba887e329092
Alexei T <turencko.alexei@yandex.ru>
2026-08-29 20:40:26 +0400

    add: base end points viwes, html, urls

---
+++ books/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+app_name = 'blog'  
+
+urlpatterns = [
+    # html-представление всех книг в виде таблицы
+    path('books/', views.list, name='list'),
+    
+    # html-представление книги id
+    path('book/<int:book_id>/', views.book_id_html, name='book_id_html'),
+  
+    # json-представление всех книг
+    path('api/books/', views.books_json, name='books_json'),
+
+    # json-представление книги id
+        path('api/book/<int:book_id>/', views.books_json_id, name='books_id'),
+]
--- books/views.py
+++ books/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpRequest, JsonResponse
+from django.forms import model_to_dict
 
-# Create your views here.
+from books.models import Book
+
+def list(request: HttpRequest) -> HttpResponse:
+
+    books = Book.objects.all().order_by('title')
+
+    context = {
+        'books': books,               
+        'title': 'cписок книг',  
+        'total_books': books.count() 
+    }
+
+    return render(request, 'books/book_list.html', context)
+
+def book_id_html(request: HttpRequest, book_id) -> HttpResponse:
+
+    book = get_object_or_404(Book, id=book_id)
+
+    context = {
+            'book': book,               
+            'title': f'Описание книги',  
+            }
+
+    return render(request, 'books/book_id.html', context)
+
+def books_json(request: HttpRequest) -> JsonResponse:
+
+    books = Book.objects.all().order_by('title')
+
+    books_data = [model_to_dict(book) for book in books]
+
+    return JsonResponse(books_data, safe=False)
+
+def books_json_id(request: HttpRequest, book_id) -> JsonResponse:
+
+    book = get_object_or_404(Book, id=book_id)
+
+    book_data = model_to_dict(book)
+
+    return JsonResponse(book_data)
